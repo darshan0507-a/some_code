cf80b98a7d1d253256efccb9b0078881229459d2
root <root@ccswagata1c.mylabserver.com>
2020-05-08 06:35:38 +0000

    I have added this to test githib hook-second attempt

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,5 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is complete"
 echo "I jave just added this for testing purpose so i can test github hook is working"
+echo "This is second attempt"
 
