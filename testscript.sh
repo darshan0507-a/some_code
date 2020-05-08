4efe082b24dde5cb2eb645159eeb607466f3a4bf
root <root@ccswagata1c.mylabserver.com>
2020-05-08 06:06:00 +0000

    I have added this to test githib hook

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is complete"
+echo "I jave just added this for testing purpose so i can test github hook is working"
+
