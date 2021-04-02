26ec3681d2a849938195d20cd08d8c64ec156bc8
root <root@ip-172-31-29-154.ec2.internal>
2021-04-02 22:18:09 +0000

    message added

--- testscript.sh
+++ testscript.sh
@@ -5,4 +5,4 @@ echo "$NUMBER"
 echo "This is complete"
 echo "I jave just added this for testing purpose so i can test github hook is working"
 echo "This is second attempt"
-
+ECHO "tHIS IS tHORD ATTAEMPT"
