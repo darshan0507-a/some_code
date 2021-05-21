e4e8c2f36aaa826cb9e78306418667d7794f2ac5
darshan <darshan@gmail.com>
2021-05-21 04:51:42 +0000

    removed lines

--- testscript.sh
+++ testscript.sh
@@ -2,8 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This is complete"
-echo "I jave just added this for testing purpose so i can test github hook is working"
-echo "This is second attempt"
-echo "tHIS IS tHORD ATTAEMPT"
-echo "This is fourth attempt"
+echo "This is the result"
+
